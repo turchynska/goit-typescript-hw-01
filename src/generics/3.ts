4dc77becf46ede021ca59d8b2164ef1a6288d3d8
Kate <159571202+turchynska@users.noreply.github.com>
2024-10-07 15:30:33 -0400

    finish

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,5 @@
 function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+    return Object.assign({},objA, objB);
   }
   const result = merge({email: 'qwerty@mail.com'}, {number: 12345431});
   console.log(result)
